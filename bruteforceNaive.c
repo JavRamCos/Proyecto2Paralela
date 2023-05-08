e1b244305e990f3bfaa9c13f6d4e2ba84a6771c4
Javier Ramirez <ram18099@uvg.edu.gt>
2023-05-08 00:51:36 -0600

    Update de archivos
    - Agregado archivo input_file.txt que contiene cadena de texto para cifrar/descifrar
    - Modifiacion a funciones encrypt y decrypt para que acepten como parametros una lista destino, una lista source y su respectiva llave
    - Modificacion de funcion tryKey para aceptar como parametros la lista con caracteres cifrados y la respectiva llave
    - Reestructuracion de funcion main para mayor legibilidad

--- bruteforceNaive.c
+++ bruteforceNaive.c
@@ -12,6 +12,7 @@
 #include <limits.h>
 #include <mpi.h>
 #include <unistd.h>
+#include <time.h>
 #include <openssl/des.h>
 
 #define INFILE "Proyecto2Paralela/input_file.txt"
@@ -51,82 +52,81 @@ int ReadFile(char* dest) {
   return 1;
 }
 
-// Generar llave para encriptacion/desencriptacion
-void GenerateKey(long key,DES_key_schedule sched) {
-  DES_set_key_unchecked((DES_cblock*)&key, &sched);
-}
-
-//descifra un texto dado una llave
-void decrypt(long key, char *ciph, int len){
-  //set parity of key and do decrypt
-  long k = 0;
-  for(int i=0; i<8; ++i){
-    key <<= 1;
-    k += (key & (0xFE << i*8));
+// descifra un texto dado una llave
+void decrypt(char* src,char* dest,DES_key_schedule sched) {
+  for(int i  = 0; i < strlen(src); i += 8) {
+    char temp[8] = { src[i], src[i+1], src[i+2], src[i+3],
+                     src[i+4], src[i+5], src[i+6], src[i+7] };        
+    char temp2[8] = {""};
+    DES_ecb_encrypt((const_DES_cblock*)temp,(DES_cblock*)temp2,&sched,DES_DECRYPT);
+    for(int k = 0; k<8; k++) {
+      dest[i+k] = temp2[k];
+    }
   }
-  des_setparity((char *)&k); //el poder del casteo y &
-  ecb_crypt((char *)&k, (char *) ciph, 16, DES_DECRYPT);
 }
 
-//cifra un texto dado una llave
-void encrypt(long key, char *ciph){
-  //set parity of key and do encrypt
-  long k = 0;
-  for(int i=0; i<8; ++i){
-    key <<= 1;
-    k += (key & (0xFE << i*8));
+// cifa un texto dado una llave
+int encrypt(char* src,char* dest,DES_key_schedule sched) {
+  if(strlen(src)-1 % 8 == 0) {
+    printf("Text does not have a length multiple of 8...\n");
+    return 0;
   }
-  des_setparity((char *)&k); //el poder del casteo y &
-  ecb_crypt((char *)&k, (char *) ciph, 16, DES_ENCRYPT);
-}
-
-//palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
-char search[] = "es una prueba de";
-int tryKey(long key, char *ciph, int len){
-  char temp[len+1]; //+1 por el caracter terminal
-  memcpy(temp, ciph, len);
-  temp[len]=0; //caracter terminal
-  decrypt(key, temp, len);
-  return strstr((char *)temp, search) != NULL;
+  for(int i  = 0; i < strlen(src); i += 8) {
+    char temp[8] = { src[i], src[i+1], src[i+2], src[i+3],
+                     src[i+4], src[i+5], src[i+6], src[i+7] };        
+    char temp2[8] = {""};
+    DES_ecb_encrypt((const_DES_cblock*)temp,(DES_cblock*)temp2,&sched,DES_ENCRYPT);
+    for(int k = 0; k<8; k++) {
+      dest[i+k] = temp2[k];
+    }
+  }
+  return 1;
 }
 
 char eltexto[INT_MAX];
-long the_key = 123456L;
-// 2^56 / 4 es exactamente 18014398509481983
-// long the_key = 18014398509481983L;
-// long the_key = 18014398509481983L + 1L;
+
+// palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
+char search[] = "dolor";
+int tryKey(char* src,DES_key_schedule sched) {
+  char temp[strlen(src)];
+  decrypt(src,temp,sched);
+  return 1;
+  // return strstr((char *)temp, search) != NULL;
+}
 
 int main(int argc, char *argv[]) {
   int N, id;
   long upper = (1L << 56); 
   long mylower, myupper;
-  MPI_Status st;
-  MPI_Request req;
 
   // Lectura de archivo
   if(ReadFile(eltexto) == 0) {
     return EXIT_FAILURE;
   }
+  // char eltexto[] = "TestTestTestTest";
 
-  int ciphlen = strlen((char *)eltexto);
-  MPI_Comm comm = MPI_COMM_WORLD;
+  // Generar llave
+  char the_key[] = "123456";
+  // 2^56 / 4 es exactamente 18014398509481983
+  // long the_key = 18014398509481983L;
+  // long the_key = 18014398509481983L + 1L;
+  DES_cblock key;
+  DES_key_schedule sched; 
+  DES_string_to_key(the_key,&key);
+  DES_set_key((const_DES_cblock*)&key,&sched); 
 
   // Cifrar el texto
-  char cipher[ciphlen + 1];
-  memcpy(cipher, eltexto, ciphlen);
-  cipher[ciphlen] = 0;
-  
-  // Generar llave
-  DES_key_schedule sched;
-  GenerateKey(the_key,sched);
-
-  // Encriptar texto
-  encrypt(the_key,cipher);
+  char ciphtext[strlen(eltexto)];
+  char finaltext[strlen(eltexto)];
+  encrypt(eltexto,ciphtext,sched);
 
   // Inicializar MPI
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(comm, &N);
-  MPI_Comm_rank(comm, &id);
+  MPI_Status st;
+  MPI_Request req;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Init(NULL,NULL);
+  MPI_Comm_size(comm,&N);
+  MPI_Comm_rank(comm,&id);
 
   long found = 0L;
   int ready = 0;
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
   long range_per_node = upper / N;
   mylower = range_per_node * id;
   myupper = range_per_node * (id + 1) - 1;
-  if(id==N-1) {
+  if(id ==N-1) {
     // Compensar residuo
     myupper = upper;
   }
@@ -147,9 +147,9 @@ int main(int argc, char *argv[]) {
   for(long i = mylower; i<myupper; ++i) {
     MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
     if(ready) {
-        break; // Ya encontraron, salir
+      break; 
     }
-    if(tryKey(i, cipher, ciphlen)) {
+    if(tryKey(ciphtext,sched)) {
       found = i;
       printf("Process %d found the key\n", id);
       for (int node = 0; node < N; node++) {
@@ -158,17 +158,19 @@ int main(int argc, char *argv[]) {
       break;
     }
   }
-
   // Wait y luego imprimir el texto
   if(id == 0) {
     MPI_Wait(&req, &st);
-    decrypt(found, cipher, ciphlen);
+    DES_key_schedule found_schedule; // Cambiado a DES_key_schedule
+    DES_set_key_unchecked((DES_cblock *)&found, &found_schedule); // Utiliza la clave encontrada (found) en lugar de the_key
+    decrypt(ciphtext,finaltext,sched);
     printf("Key = %li\n\n", found);
-    printf("Text: %s\n", cipher);
+    printf("Texto original: %s\n",eltexto);
+    printf("Texto cifrado: %s\n",ciphtext);
+    printf("Texto descifrado: %s\n",finaltext);
   }
   printf("Process %d exiting\n", id);
-
-  // FIN entorno MPI
+  // Finalizar entorno MPI
   MPI_Finalize();
   return EXIT_SUCCESS;
 }
